453a0e8b121f88bdbbb2765806aab002e7e8c995
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-06-16 20:50:11 -0700

    Nuke file after test

--- database.test.c
+++ database.test.c
@@ -3,18 +3,19 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 // You've really fucking done it now, Mark.
 // Not impressed.
-const static char* SAMPLE_PATH = "features/facebook-crypto-currency";
+const static char* SAMPLE_PATH = "features-facebook-crypto-currency";
 
 int databasePutGetTest() {
+  int size = 1024;
   struct TimeEnty_t input = {
     .direction = IN,
     .datetime = time(NULL)
   };
   struct TimeEnty_t output[1024];
-  int size = 1024;
   database_pushEntry(SAMPLE_PATH, &input);
   database_getEntries(SAMPLE_PATH, output, &size);
   if(size != 1) {
@@ -26,8 +27,10 @@ int databasePutGetTest() {
   if(output[0].datetime != input.datetime) {
     goto fail;
   }
+  database_nuke(SAMPLE_PATH);
   return 0;
 fail:
+  database_nuke(SAMPLE_PATH);
   return -1;
 }
 

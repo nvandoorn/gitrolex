00dad6a75d1eebd853f294425ff588743b62368c
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-06-16 20:06:08 -0700

    Test database

--- database.test.c
+++ database.test.c
@@ -1,8 +1,37 @@
-
 #include "test-lib.h"
 #include "database.h"
 
-
 #include <stdlib.h>
 #include <stdio.h>
+
+// You've really fucking done it now, Mark.
+// Not impressed.
+const static char* SAMPLE_PATH = "features/facebook-crypto-currency";
+
+int databasePutGetTest() {
+  struct TimeEnty_t input = {
+    .direction = IN,
+    .datetime = time(NULL)
+  };
+  struct TimeEnty_t output[1024];
+  int size = 1024;
+  database_pushEntry(SAMPLE_PATH, &input);
+  database_getEntries(SAMPLE_PATH, output, &size);
+  if(size != 1) {
+    goto fail;
+  }
+  if(output[0].direction != input.direction) {
+    goto fail;
+  }
+  if(output[0].datetime != input.datetime) {
+    goto fail;
+  }
+  return 0;
+fail:
+  return -1;
+}
+
+void database_test() {
+  syncTest("database_pushEntry and database_getEntries", "Failed to put then get", databasePutGetTest);
+}
 
--- database.test.h
+++ database.test.h
@@ -2,9 +2,9 @@
 #ifndef GITROLEX_DATABASE_H_TEST
 #define GITROLEX_DATABASE_H_TEST
 
-
 #include <stdlib.h>
 #include <stdio.h>
 
+void database_test();
 
 #endif
